62244848ed9383ba094c045aa497ef5a16f5cd02
Claudiasals <salsiniclaudia@gmail.com>
2026-01-06 01:59:57 +0100

    es.6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+/* il doppio asterisco è uncommento speciale che serve a dare tipo e suggerimenti all’editor. 
+In pratica dice: “Ehi, questo file è un oggetto di configurazione Tailwind”. */
+
 export default {
     theme: {
         extend: {
+            colors: {
+                'brand-purple': '#9b5de5', // titoli
+                'brand-blue': '#1e40af',    // testo principale blu scuro
+            },
+            fontFamily: {
+                'brand': ['Poppins', 'sans-serif'], // font personalizzato
+            },
             spacing: {
-                '32': '8rem',  // p-72 (per padding ) o m-72 ( per margin)
+                '32': '8rem',  // p-32 / m-32 come esempio
             },
         },
     },
     plugins: [],
 }
 
+
 /*
-p-32 → padding su tutti e 4 i lati = 18rem
-m-32 → margin su tutti e 4 i lati = 18rem
-mt-32 → margin-top = 18rem
-mb-32 → margin-bottom = 18rem
-px-32 → padding orizzontale (left + right) = 18rem
-py-32 → padding verticale (top + bottom) = 18rem
+p-32 → padding su tutti e 4 i lati = 8rem
+m-32 → margin su tutti e 4 i lati = 8rem
+mt-32 → margin-top = 8rem
+mb-32 → margin-bottom = 8rem
+px-32 → padding orizzontale (left + right) = 8rem
+py-32 → padding verticale (top + bottom) = 8rem
 */
